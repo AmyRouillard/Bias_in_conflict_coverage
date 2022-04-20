cc0bc7187ae4025afe05a6c396007838f7d5e0bb
AmyRouillard <rouillardamy@gmail.com>
2022-04-20 06:56:06 +0000

    app update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ path = os.path.dirname(__file__)
 
 
 
-st.markdown("# Bias in media coverage of severe conflicts")
+st.markdown("# Bias in media coverage of severe conflicts between May 2019 and April 2022")
 
 # -------------
 # 
@@ -38,7 +38,7 @@ df = pd.read_pickle(path+'/df_2.pkl')
 
 fig = px.choropleth(df, locations="iso3",
                     color="fatalities",
-                    title = "Accumulated fatalities for conflicts of interest",
+                    title = "Accumulated fatalities for regions of interest",
                     color_continuous_scale=px.colors.sequential.PuRd,
                     range_color = (df["fatalities"].min(),df["fatalities"].max()))
 
@@ -52,7 +52,7 @@ df = pd.read_pickle(path+'/df_3.pkl')
 
 fig = px.choropleth(df, locations="iso3",
                     color="fatalities",
-                    title = "Accumulated fatalities",
+                    title = "Accumulated fatalities world wide",
                     color_continuous_scale=px.colors.sequential.PuRd,
                     range_color = (df["fatalities"].min(),df["fatalities"].max()))
 
@@ -62,16 +62,19 @@ st.plotly_chart(fig, use_container_width=True)
 # -------------
 # 
 # -------------
+
+st.markdown("## Fatalities and media coverage")
+
 df = pd.read_pickle(path+'/df_4.pkl')
 
-# for c in df['country'].unique():
-#     agree = st.checkbox(c)
+
 
 df = df.sort_values(by=['country'])
 
 fig = px.scatter(df, x="event_date_datetime", y="fatalities", color="country",# symbol="event_type",
                  size='fatalities',
-                  hover_data=['country','event_type'], labels=dict(event_date_datetime="Event date", fatalities="Fatalities", country="Country", event_type ="Event type"))
+                 hover_data=['country','event_type'], labels=dict(event_date_datetime="Event date", 
+                fatalities="Fatalities", country="Country", event_type ="Event type"))
 
 st.plotly_chart(fig, use_container_width=True)
 
@@ -83,7 +86,12 @@ df = pd.read_pickle(path+'/df_5.pkl')
 df = df.sort_values(by=['country'])
 
 fig = px.scatter(df, x="date", y="AccNumArticles", color="country",
-                 labels=dict(date="Date", AvgTone="Average Tone", country="Country", AccNumArticles ="Accumulated Number of articles"))
+                 labels=dict(date="Date", AvgTone="Average Tone", country="Country", AccNumArticles ="Accumulated number of articles"))
 st.plotly_chart(fig, use_container_width=True)
 
 
+fig = px.scatter(df, x="date", y="AvgTone", color="country",
+                 labels=dict(date="Date", AvgTone="Average Tone", country="Country", AccNumArticles ="Accumulated number of articles"))
+
+
+st.plotly_chart(fig, use_container_width=True)
